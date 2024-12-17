97b21d224bf8be383b048dcc2f129cabb1f9133e
n4vermind <dnsraaa@gmail.com>
2024-12-17 15:57:04 +0700

    W13: Jawaban Soal 12
    getNumbers berfungsi menghasilkan stream yang memproduksi angka acak antara 0 hingga 9 setiap detik.
    
    StreamBuilder berfungsi untuk membangun UI yang responsif terhadap perubahan data dari stream numberStream. Ia menampilkan angka acak yang dihasilkan setiap detik dengan nilai awal 0, dan menangani jika terjadi error.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+
+import 'stream.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +13,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Stream',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const StreamHomePage(),
+    );
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
+
+  @override
+  State<StreamHomePage> createState() => _StreamHomePageState();
+}
+
+class _StreamHomePageState extends State<StreamHomePage> {
+  late Stream<int> numberStream;
+
+  @override
+  void initState() {
+    numberStream = NumberStream().getNumbers();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream'),
       ),
+      body: StreamBuilder(
+          stream: numberStream,
+          initialData: 0,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print('Error!');
+            }
+            if (snapshot.hasData) {
+              return Center(
+                child: Text(
+                  snapshot.data.toString(),
+                  style: const TextStyle(fontSize: 96),
+                ),
+              );
+            } else {
+              return const SizedBox.shrink();
+            }
+          }),
     );
   }
 }
