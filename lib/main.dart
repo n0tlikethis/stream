80c9e4fc481b9ce9a6d8aa5deb4a4576ad369929
n4vermind <dnsraaa@gmail.com>
2024-12-17 08:43:55 +0700

    W13: Jawaban Soal 1

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Stream - Dani Daneswara',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const StreamHomePage(),
     );
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
+
+  @override
+  State<StreamHomePage> createState() => _StreamHomePageState();
+}
+
+class _StreamHomePageState extends State<StreamHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
